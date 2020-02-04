c037969d8f495bfd47c713feda45e781c566d48a
Syrone Wong <wong.syrone@gmail.com>
2020-02-04 22:57:52 +0800

    update from upstream

--- start-xuexi-flaskr-api.bat
+++ start-xuexi-flaskr-api.bat
@@ -5,4 +5,9 @@ set PYTHONDONTWRITEBYTECODE=1
 
 python -m flask run
 
+:: use shell context to load/dump
+::curl -L -O http://49.235.90.76:5000/api/questions
+::ren questions data-input.json
+::python -m flask shell
+
 pause & break
